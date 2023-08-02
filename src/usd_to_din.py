176722dbe695011c30b01a1267370326d515f32c
Ivan Miletic <milemik68@gmail.com>
2023-08-02 17:05:29 +0200

    Added round to calculation
    Added round calculation

--- src/usd_to_din.py
+++ src/usd_to_din.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from decimal import ROUND_DOWN, Decimal
 
 
 @dataclass
@@ -8,7 +9,8 @@ class KursVrednost:
     calculated_value: float = 0.0
 
     def calculate_value(self) -> float:
-        self.calculated_value = self.srednji_kurs * self.user_value
+        calculated_curs_value = self.srednji_kurs * self.user_value
+        self.calculated_value =  Decimal(calculated_curs_value).quantize(Decimal('.001'))
 
     def to_representation(self):
         print(f"{self.user_value} iznosi {self.calculated_value} dinara")
